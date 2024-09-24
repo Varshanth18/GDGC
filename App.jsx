31214d9dacece756b134c62abc1e5e11af52a888
Varshanth18 <varshanth1807@gmail.com>
2024-09-25 00:55:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+// import Registration from './components/Assignment-4/Registration'
+// import One from './components/Assignment-2/one'
+// import Three1 from './components/Assignment-2/3rd/Navbar'
+// import Two from './components/Assignment-2/Two'
+// import TwoItems from './components/Assignment-2/TwoItems'
+// import TwoItems1 from './components/Assignment-2/TwoItems1'
+// import Navbar from './components/Assignment-2/3rd/Navbar'
+// import UsersList1 from './components/Assignment-2/3rd/Userslist1'
+// import UserList2 from './components/Assignment-2/3rd/UserList2'
+// import Footer from './components/Assignment-2/3rd/Footer'
+// import Nav from './components/Assignment-2/3.5/Nav'
+// import Profiles from './components/Assignment-2/3.5/Profiles'
+// import FormDemo1 from './components/Assingnment-3/Formdemo1'
+import A from './components/Assingnment-3/A'
+import B from './components/Assingnment-3/B'
+import Registration from './components/Assignment-4/Registration'
+import Header from './components/GDGC/Header'
+import Products from './components/GDGC/Products'
+
+
+
+
+function App() {
+
+  return (
+    <div>
+      {/* <Header /> */}
+      <Products />
+    </div>
+
+  )
+} 
+
+export default App
---
+++ Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Header.css"
+
+function Header() {
+
+    return (
+        <div className='nav p-4 shadow align-items-center mb-5'>
+            
+            <h2 className=' mx-auto'>ECOMMERCE WEBSITE</h2>
+        </div>
+    )
+
+}
+
+export default Header
